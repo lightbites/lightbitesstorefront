d324bb57f476fb2421c99bb5b5d3a05745ac16f8
owaisj <jamil.owais@gmail.com>
2019-06-18 16:35:17 -0500

    Customer model based on seeds.sql

--- models/customer.js
+++ models/customer.js
@@ -1,14 +1,15 @@
-// Customer model
-
-// The Customer has a "customer" attribute of type DataTypes.String
-
 module.exports = function(sequelize, DataTypes) {
   var Customer = sequelize.define("Customer", {
-    customer: {
-      type: DataTypes.STRING,
-      // If a customer is to be created, they must have a name
-      allowNull: false
-    }
+    firstname: DataTypes.STRING,
+    lastname: DataTypes.STRING,
+    phonenumber: DataTypes.STRING,
+    email: DataTypes.STRING,
+    address1: DataTypes.STRING,
+    address2: DataTypes.STRING,
+    address3: DataTypes.STRING,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING,
+    zip: DataTypes.STRING
   });
   return Customer;
 };
