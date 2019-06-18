ad014bfd34632b9117c9a9d73b863bc9342740c6
Krayt0n3 <hendersonkevin64@gmail.com>
2019-06-18 10:20:39 -0500

    more design work done for the profile page. Now you can pass the values into the card component so it will show up in the personalized profile. We can fetch data from the database and pass the returned data as props to display in each card.

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -3,27 +3,39 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Header from "../../components/Header-Welcome";
+import ProfileCard from "../../components/Profile-Card";
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
+  },
+  container: {
+    marginTop: "5%"
   },
   paper: {
     padding: theme.spacing(2),
     textAlign: "center",
     color: theme.palette.text.secondary,
   },
+  text: {
+    textAlign: "left"
+  }
 }));
 
 export default function Profile() {
   const classes = useStyles();
-  
+  ///fetch user here///
   return (
     <div className={classes.root}>
       <Header />
       
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
+      <Grid className={classes.container} container spacing={3}>
+        <Grid item xs={6}>
+          <ProfileCard 
+            name={"John Jerome"}
+            calories={"2,145"}/>
+        </Grid>
+        <Grid item xs={6}>
           <Paper className={classes.paper}>xs=12</Paper>
         </Grid>
         <Grid item xs={6}>
