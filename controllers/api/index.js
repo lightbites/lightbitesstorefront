afc5a313cee8f8627666c3d8feeef5fff2a94f14
owaisj <jamil.owais@gmail.com>
2019-06-21 12:01:40 -0500

    Booked stock route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 //Sequelize Routes
 const customerRoutes = require('./customer');
+const stockRoutes = require('./stockRoutes')
 
 // Book routes
-router.use('/customers', customerRoutes)
+router.use('/customers', customerRoutes);
+router.use('/stock', stockRoutes);
 module.exports = router;
