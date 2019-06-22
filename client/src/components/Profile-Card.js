3dc464fa0a72c6c7369ca67500be5a5f81d19a00
owaisj <jamil.owais@gmail.com>
2019-06-22 11:54:04 -0500

    Grabbing and displaying data

--- client/src/components/Profile-Card.js
+++ client/src/components/Profile-Card.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
@@ -23,7 +23,8 @@ const useStyles = makeStyles({
   },
 });
 
-export default function ProfileCard(props) {
+//NOTE: This is the old version of the component
+function ProfileCard(props) {
   const classes = useStyles();
   return (
     <Card className={classes.card}>
@@ -48,3 +49,64 @@ export default function ProfileCard(props) {
   );
 }
 
+class ProfileTest extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userInfo: {
+        fname: '',
+        lname: '',
+        email: '',
+        address1: '',
+        city: '',
+        state: ''
+      }
+    };
+    this.grabInformation = this.grabInformation.bind(this);
+  }
+  grabInformation (attr, val) {
+    fetch(`/api/customers/${attr}/${val}`, {
+        method: 'GET'
+    })
+    .then(res => res.json())
+    .then(json => {
+      let u = json[0];
+      this.setState({
+        userInfo: {
+          fname: u.firstname,
+          lname: u.lastname,
+          address1: u.address1,
+          city: u.city,
+          state: u.state
+        }
+      })
+      console.log(this.state);
+    });
+  }
+  render() {
+    let profile = this.state.userInfo;
+    return (
+      <div>
+        <ul>
+          <li>
+            {profile.lname}, {profile.fname}
+          </li>
+          <li>
+            {profile.address1}
+          </li>
+          <li>
+            {profile.city}, {profile.state}
+          </li>
+        </ul>
+        <Button onClick={() => this.grabInformation('email', 'owais@mail.com')}>
+          Refresh Your Profile
+        </Button>
+      </div>
+    )
+  }
+}
+
+export default ProfileTest
+
+//TODO: DisplayProfile - GET from db
+//From Login -> email, <value>
